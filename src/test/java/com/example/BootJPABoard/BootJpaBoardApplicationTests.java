2674932aa7b0666feee537522bc7f72f94512e99
Hongrokgi <Hongrokgi@naver.com>
2022-06-20 13:18:25 +0900

    init commit

---
+++ src/test/java/com/example/BootJPABoard/BootJpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BootJPABoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
